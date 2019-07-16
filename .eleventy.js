8889104e19cdb176e5884654a3e23a66740789da
Mathieu <mathieu.huot@gmail.com>
2019-07-16 11:31:10 -0400

    Converted project to eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,8 @@
 const sass = require('sass');
 const fs = require('fs');
-const missingDir = './docs/style';
 const scssFile = './style/index.scss';
 const cssFile =  './docs/style/index.css';
+const missingDir = './docs/style';
 const assets = [
     'images',
     'fonts',
@@ -10,12 +10,16 @@ const assets = [
 ]
 
 module.exports = config => {
+    //Passing assets as is to docs directory
     assets.forEach(asset => config.addPassthroughCopy(asset));
-
+    
     fs.watch('./style', () => {
+        //Returning css from scssFile
         const result = sass.renderSync({file: scssFile});
+        //Then writing result to cssFile
         fs.writeFile(cssFile, result.css.toString(), err => {
             if(err) {
+                //Creates ./docs/style if it's missing
                 if(err.code === 'ENOENT') {
                     fs.mkdir(missingDir, err => {
                      console.error(`mkdir failed with : ${err}`);
