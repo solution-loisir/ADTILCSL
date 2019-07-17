8e457ed04764c4c0a3399df43dc0d0ccdc993d62
Mathieu <mathieu.huot@gmail.com>
2019-07-17 15:30:44 -0400

    Test

--- .eleventy.js
+++ .eleventy.js
@@ -33,6 +33,8 @@ module.exports = config => {
         dir: {
             output: 'docs',
             pathPrefix: '/ADTILCSL/'
-        }
+        },
+        dataTemplateEngine: 'njk',
+        htmlTemplateEngine: 'njk'
     }
 }
