e23b70a380a3819126e773fe010577dd201989ea
Mathieu <mathieu.huot@gmail.com>
2019-10-10 15:37:25 -0400

    Refactored sass-process.js

--- config/sass-process.js
+++ config/sass-process.js
@@ -3,15 +3,20 @@ const fs = require('fs-extra');
 const path = require('path');
 
 module.exports = (scssPath, cssPath) => {
+    //Encapsulate rendered css from scssPath into result variable
+    const result = sass.renderSync({file: scssPath});
+    //If cssPath directory doesn't exist...
+    if(!fs.existsSync(path.dirname(cssPath))) {
+        //Create cssPath directory recursively
+        fs.mkdir(path.dirname(cssPath), {recursive: true})
+        //Then write result css string to cssPath file
+        .then(() => fs.writeFile(cssPath, result.css.toString()))
+        .catch(error => console.error(error))
+    }
+    //Watch for changes to scssPath directory...
     fs.watch(path.dirname(scssPath), () => {
         console.log(`Watching ${path.dirname(scssPath)}...`);
-
-        const result = sass.renderSync({file: scssPath});
-
-        if(!fs.existsSync(path.dirname(cssPath))) {
-            fs.mkdir(path.dirname(cssPath))
-            .catch(error => console.error(error))
-        } 
+        //Then write result css string to cssPath file
         fs.writeFile(cssPath, result.css.toString())
         .catch(error => console.error(error))      
     });
