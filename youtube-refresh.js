c4ab5aec12b4711fed0b84275b7c527a4375a4d1
Mathieu <mathieu.huot@gmail.com>
2020-04-25 14:48:04 -0400

    Add netlify.toml

--- youtube-refresh.js
+++ youtube-refresh.js
@@ -1,14 +1,19 @@
 const https = require('https');
 const fs = require('fs');
+const { pipeline } = require('stream');
 require('dotenv').config();
 
+//Creating Writable stream with a file target
 const OUTPUT = fs.createWriteStream('./_src/_data/youtube.json');
 
 https.get(`https://www.googleapis.com/youtube/v3/search?part=id&channelId=${process.env.YOUTUBE_CHANNELID}&type=video&maxResults=20&order=date&key=${process.env.YOUTUBE_KEY}`, res => {
-    res.pipe(OUTPUT)
-    .on('error', error => {
-        console.error(error);
-        //Ending writable stream on error
-        this.end();
-    });
-}).on('error', error => console.error(error));
+    //Piping res (IncomingMessage: Readable) to OUTPUT (Writable)
+    //The .pipe method wilL manage stream flow and errors!
+    pipeline(res, OUTPUT, error => {
+        if(error) {
+            console.error(error.stack);
+        } else {
+            console.log('Tranfering YouTube data is done!');
+        }
+    });  
+}).on('error', error => console.error(error.stack));
