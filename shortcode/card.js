1c8be811429b2b41f98018107a2f6218205de79f
Mathieu <mathieu.huot@gmail.com>
2020-04-28 15:18:30 -0400

    Developing design

--- shortcode/card.js
+++ shortcode/card.js
@@ -1,4 +1,4 @@
-module.exports = ({cardClass, svg, svgText, title, text, link, linkText = 'En savoir plus!'} = {}) => {
+module.exports = ({cardClass, svg = '', svgText = '', title, text, link, linkText = 'En savoir plus!'} = {}) => {
     return `
 <section class="card ${cardClass}">
 <object type="image/svg+xml" data="${svg}" class="card-svg">${svgText}</object>
