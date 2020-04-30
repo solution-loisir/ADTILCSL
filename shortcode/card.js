31c2bc214c63cb1c01c1c78c2dc2b787513e8a39
Mathieu <mathieu.huot@gmail.com>
2020-04-30 13:48:46 -0400

    Removed min-width and made card clikable

--- shortcode/card.js
+++ shortcode/card.js
@@ -1,10 +1,12 @@
-module.exports = ({cardClass, svg, svgText, title, time, text, link, linkText = 'En savoir plus!'} = {}) => {
-    return `
+module.exports = ({cardClass, svg, alt, title, time, text, link, linkText = 'En savoir plus!'} = {}) => {
+    return ` 
 <div class="card ${cardClass}">
-${svg ? `<object type="image/svg+xml" data="${svg}" class="card-svg">${svgText}</object>` : ''}
+${svg && link ? `<a class="card-full-anchor" href="${link}">` : ''}
+${svg ? `<object type="image/svg+xml" data="${svg}" class="card-svg">${alt}</object>` : ''}
 <h2>${title}</h2>
 ${time ? `<p><small>${time}</small></p>` : ''}
 <p>${text}</p>
-<a href="${link}">${linkText}</a>
+${svg && link ? `</a>` : ''}
+<a class="card-anchor" href="${link}">${linkText}</a>
 </div>`;
 }
