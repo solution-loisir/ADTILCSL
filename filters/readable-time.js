e140f1420fa9499c963248ac169016e2fb76008a
Mathieu <mathieu.huot@gmail.com>
2020-05-08 21:33:26 -0400

    Refactored readable-time filter

--- filters/readable-time.js
+++ filters/readable-time.js
@@ -1,15 +1,1 @@
-module.exports = date => new Date(date)
-    .toLocaleString('fr-CA', {timeZone: 'utc', year: 'numeric' , month: 'long', day: 'numeric'});
-    /*.replace(/([0-9]{4}) (M[0-9]{2}) ([0-9]{1,2})/, '$3 $2 $1')
-    .replace(/M01/, 'jan.')
-    .replace(/M02/, 'févr.')
-    .replace(/M03/, 'mars')
-    .replace(/M04/, 'avr.')
-    .replace(/M05/, 'mai')
-    .replace(/M06/, 'juin')
-    .replace(/M07/, 'juil.')
-    .replace(/M08/, 'août')
-    .replace(/M09/, 'sept.')
-    .replace(/M10/, 'oct.')
-    .replace(/M11/, 'nov.')
-    .replace(/M12/, 'déc.');*/
+module.exports = date => new Date(date).toLocaleString('fr-CA', {timeZone: 'utc', year: 'numeric' , month: 'long', day: 'numeric'});
