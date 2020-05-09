0c336e2f6f68502e2db52a4ce5f95ba32e6cbb36
Mathieu <mathieu.huot@gmail.com>
2020-05-08 21:21:37 -0400

    removed replace methods in readable-time filter

--- filters/readable-time.js
+++ filters/readable-time.js
@@ -1,6 +1,6 @@
 module.exports = date => new Date(date)
-    .toLocaleString('fr-CA', {timeZone: 'utc', year: 'numeric' , month: 'long', day: 'numeric'})
-    .replace(/([0-9]{4}) (M[0-9]{2}) ([0-9]{1,2})/, '$3 $2 $1')
+    .toLocaleString('fr-CA', {timeZone: 'utc', year: 'numeric' , month: 'long', day: 'numeric'});
+    /*.replace(/([0-9]{4}) (M[0-9]{2}) ([0-9]{1,2})/, '$3 $2 $1')
     .replace(/M01/, 'jan.')
     .replace(/M02/, 'févr.')
     .replace(/M03/, 'mars')
@@ -12,4 +12,4 @@ module.exports = date => new Date(date)
     .replace(/M09/, 'sept.')
     .replace(/M10/, 'oct.')
     .replace(/M11/, 'nov.')
-    .replace(/M12/, 'déc.');
+    .replace(/M12/, 'déc.');*/
