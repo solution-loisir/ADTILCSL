6d3e0db41a00cc3e0a94875683a44db7b6d9f0b9
Joanie Pilon-Proulx <JoPP@macbook-de-joanie-pilon-proulx-2.local>
2020-09-29 11:11:44 -0400

    Refactored lazyload function with a negative condition for detecting IntersectionObserver.

--- js/index.js
+++ js/index.js
@@ -19,33 +19,27 @@ if(document.getElementById('header-btn')) {
 (() => {
     const images = document.querySelectorAll('[data-src]');
     const setSrc = target => target.src = target.dataset.src;
-    //Detecting IntersectionObserver
-    if('IntersectionObserver' in window) {
-        const options = {
-        root: null,
-        rootMargin: undefined,
-        threshold: 0
-        }
-        //Callback function of IntersectionObserver
-        const showImage = entries => {
-            entries.forEach(entry => {
-                if(entry.isIntersecting) {
-                    const target = entry.target;
-                    setSrc(target);
-                    Observer.unobserve(target);
-                }
-            });
-        }
-        //IntersectioObserver constructor
-        const Observer = new IntersectionObserver(showImage, options);
-        //Using the Observer constructor to load images
-        images.forEach(image => {
-            Observer.observe(image);
+    //Detecting IntersectionObserver and calling setSrc to load images eagerly as a fallback.
+    if(!'IntersectionObserver' in window) {
+        return images.forEach(image => setSrc(image));
+    }
+    const options = {
+    root: null,
+    rootMargin: undefined,
+    threshold: 0
+    }
+    //Callback function of IntersectionObserver
+    const showImage = entries => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting) {
+                const target = entry.target;
+                setSrc(target);
+                Observer.unobserve(target);
+            }
         });
-    } else {
-        //Calling setSrc to load images eagerly as a fallback
-        images.forEach(image => {
-            setSrc(image);  
-        });
-    } 
+    }
+    //IntersectioObserver constructor
+    const Observer = new IntersectionObserver(showImage, options);
+    //Using the Observer constructor to load images
+    images.forEach(image => Observer.observe(image));
 })();
