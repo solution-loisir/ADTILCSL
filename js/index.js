d98f0b3102ff2f0f4610b265dfb45070693f36d9
Joanie Pilon-Proulx <JoPP@macbook-de-joanie-pilon-proulx-2.local>
2020-09-29 11:45:08 -0400

    Refactored display-main-nav code with negative conditions

--- js/index.js
+++ js/index.js
@@ -2,20 +2,20 @@
 
 //Showing and hiding main-nav on small screen
 (() => {
-    if(document.getElementById('header-btn')) {
-        //Toggle display-main-nav class when menu header-btn is clicked
-        const mainNav = document.getElementById('main-nav');
-        const toggleMainNav = () =>  mainNav.classList.toggle('display-main-nav');
-        const headerBtn = document.getElementById('header-btn');
-        headerBtn.addEventListener('click', toggleMainNav);
-        //If click on main or .hero, remove display-main-nav class from main-nav
-        const main = document.querySelector('main');
-        const removeDisplayMainNavClass = () => mainNav.classList.remove('display-main-nav');
-        main.addEventListener('click', removeDisplayMainNavClass);
-        if(!document.querySelector('.hero')) return;
-        const hero = document.querySelector('.hero');
-        hero.addEventListener('click', removeDisplayMainNavClass);
-    }
+    if(!document.getElementById('header-btn')) return;
+    //Toggle display-main-nav class when menu header-btn is clicked
+    const mainNav = document.getElementById('main-nav');
+    const toggleMainNav = () =>  mainNav.classList.toggle('display-main-nav');
+    const headerBtn = document.getElementById('header-btn');
+    headerBtn.addEventListener('click', toggleMainNav);
+    //If click on main or .hero, remove display-main-nav class from main-nav
+    const main = document.querySelector('main');
+    const removeDisplayMainNavClass = () => mainNav.classList.remove('display-main-nav');
+    main.addEventListener('click', removeDisplayMainNavClass);
+    if(!document.querySelector('.hero')) return;
+    const hero = document.querySelector('.hero');
+    hero.addEventListener('click', removeDisplayMainNavClass);
+    
 })();
 //lazy load all images with data-src attribute
 (() => {
