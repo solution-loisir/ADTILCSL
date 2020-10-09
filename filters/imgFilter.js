7e47ce193f4b81807ffe550e04f32b1ca1beedcc
Joanie Pilon-Proulx <JoPP@macbook-de-joanie-pilon-proulx-2.local>
2020-10-09 09:59:03 -0400

    Refactored syntax

--- filters/imgFilter.js
+++ filters/imgFilter.js
@@ -1,7 +1,7 @@
 const sharpProcess = require('../build-process/sharp-img-process');
 
 module.exports = (input, width, alt, lazy, cb) => {
-    return sharpProcess({input, width, alt, lazy})
+    return sharpProcess({ input, width, alt, lazy })
     .then(string => cb(null, string))
     .catch(error => console.error(error));
 }
--- shortcode/img.js
+++ shortcode/img.js
@@ -1,5 +1,6 @@
 const sharpProcess = require('../build-process/sharp-img-process');
 
-module.exports = ({input, width, alt, lazy}) => {
-    return sharpProcess({input, width, alt, lazy});
+module.exports = ({ input, width, alt, lazy }) => {
+    return sharpProcess({ input, width, alt, lazy })
+    .catch(error => console.error(error));
 }
