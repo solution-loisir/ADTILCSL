2b489ed075f58dc07af025ff42fdfe9f7ed06d15
Mathieu <mathieu.huot@gmail.com>
2020-11-03 15:27:50 -0500

    Adjusted prod npm script to process assets and data.

--- youtube-refresh.js
+++ youtube-refresh.js
@@ -1,25 +1,22 @@
-const { get } = require('https');
-const { createWriteStream } = require('fs');
-const { pipeline } = require('stream');
+const { get } = require("https");
+const { createWriteStream } = require("fs");
+const { pipeline } = require("stream");
 const site = require("./_src/_data/site")();
-require('dotenv').config();
+require("dotenv").config();
 
-const formationOUTPUT = createWriteStream('./_src/_data/formationYoutube.json');
-const blogOUTPUT = createWriteStream('./_src/_data/blogYoutube.json');
+const formationOUTPUT = createWriteStream("./_src/_data/formationYoutube.json");
+const blogOUTPUT = createWriteStream("./_src/_data/blogYoutube.json");
 
 async function getPlaylist(id, output, key = process.env.YOUTUBE_KEY) {
     get(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${id}&type=video&maxResults=20&order=date&key=${key}`, res => {
         pipeline(res, output, error => {
-            if(error) {
-                console.error(error.stack);
-            } else {
-                console.log('Transfering YouTube data is done!');
-            }
+            if(error) console.error("Error in getPlaylist pipeline.", error.stack);
         });  
-    }).on('error', error => console.error(error.stack));
+    }).on("error", error => console.error("Error in getPlaylist GET request.", error.stack));
 }
 Promise.all([
     getPlaylist(site.blogList, blogOUTPUT),
     getPlaylist(site.formationList, formationOUTPUT)
 ])
-.catch(error => console.error("Error in getPlayList: ", error.stack));
+.then(console.log("Done transfering YouTube data!"))
+.catch(error => console.error("Error in getPlayList promises.", error.stack));
