fa25adfdb8ea920a1601dff53d7acaec7c7757f5
Mathieu <mathieu.huot@gmail.com>
2020-11-05 10:32:58 -0500

    Replace unicode on tab text with nunjucks replace filter

--- filters/text-format.js
+++ filters/text-format.js
@@ -1,9 +1,8 @@
-module.exports = text => {
-    return text.replace(/\n/g, ' ').toLowerCase()
-    .replace(/<.*?>/g, '')
-    .replace(/\b(je|tu|il|elle|nous|vous|ils|elles|le|la|les|de|des|dont|te|ta|ton|tes|t'en|en|un|une|on|ont|a|eu|ai|ais|ait|est|c'est|s'est|ces|ses|ce|se|pour|si|de|et|ou|sauf|moi|dans|sur|mais|oui|non|pas)\b/g, '')
-    .replace(/l'|d'|à/g, '')
-    .replace(/\.|,|\?|\(|\)|!|/g, '')
-    .replace(/ {2,}/g, ' ')
-    .replace(/(^ | $)/g, '');
-}
+module.exports = text => text.toString()
+    .toLowerCase()
+    .normalize('NFD')
+    .trim()
+    .replace( /[\u0300-\u036f]/g, '') 
+    .replace(/\s+/g, '-')
+    .replace(/[^\w\-]+/g, '')
+    .replace(/\-\-+/g, '-');
