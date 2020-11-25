903062a6cc0c1ff7f28d5ac48d740ae954afcd48
Mathieu <mathieu.huot@gmail.com>
2020-11-25 15:44:43 -0500

    Replace dev env with process.argv and updated test-prod script.

--- build-process/sass-process.js
+++ build-process/sass-process.js
@@ -2,18 +2,16 @@ const { writeFile, watch } = require('fs-extra');
 const { dirname } = require('path');
 const sassRender = require('./sass-render');
 const mkdirIfNotExist = require('./mkdir-if-not-exist');
-require('dotenv').config();
 
-module.exports = (scssPath, cssPath) => {
-    Promise.all([mkdirIfNotExist(cssPath), sassRender(scssPath, cssPath)])
+module.exports = function(scssPath, cssPath) {
+    Promise.all([ mkdirIfNotExist(cssPath), sassRender(scssPath, cssPath) ])
     .then(result => writeFile(cssPath, result[1].css.toString()))
     .catch(error => console.error(error.stack));
 
-    if(process.env.ELEVENTY_ENV === 'dev') {
-        watch(dirname(scssPath), () => {
-            sassRender(scssPath, cssPath)
+    if(process.argv.includes('--serve')) {
+        watch(dirname(scssPath), () => sassRender(scssPath, cssPath)
             .then(result => writeFile(cssPath, result.css.toString()))
-            .catch(error => console.error(error.stack));
-        });
+            .catch(error => console.error(error.stack))
+        );
     }
 }
