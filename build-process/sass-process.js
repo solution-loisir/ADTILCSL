34225d2d30f35f19e49fddfa48587c982cf245c1
Mathieu <mathieu.huot@gmail.com>
2020-11-25 15:03:01 -0500

    Changed processSass function name to sassRender.

--- build-process/sass-process.js
+++ build-process/sass-process.js
@@ -1,17 +1,17 @@
 const { writeFile } = require('fs-extra');
 const { dirname } = require('path');
-const processSass = require('./sass-render');
+const sassRender = require('./sass-render');
 const mkdirIfNotExist = require('./mkdir-if-not-exist');
 require('dotenv').config();
 
 module.exports = (scssPath, cssPath) => {
-    Promise.all([mkdirIfNotExist(cssPath), processSass(scssPath, cssPath)])
+    Promise.all([mkdirIfNotExist(cssPath), sassRender(scssPath, cssPath)])
     .then(result => writeFile(cssPath, result[1].css.toString()))
     .catch(error => console.error(error.stack));
 
     if(process.env.ELEVENTY_ENV === 'dev') {
         fs.watch(dirname(scssPath), () => {
-            processSass(scssPath, cssPath)
+            sassRender(scssPath, cssPath)
             .then(result => writeFile(cssPath, result.css.toString()))
             .catch(error => console.error(error.stack));
         });
