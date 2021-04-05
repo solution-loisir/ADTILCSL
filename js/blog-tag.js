4d419c4792728aabadeac30e87e22b9844daad9d
Mathieu Huot <mathieu.huot@gmail.com>
2021-04-04 23:12:13 -0400

    Added the popstate event handler.

--- js/blog-tag.js
+++ js/blog-tag.js
@@ -1,34 +1,44 @@
 "use strict";
 
-const listingSection = document.querySelector(".listing-section");
-const tagContainer = document.querySelector(".tags-container");
-
 const clean = element => element.innerHTML = "";
 
-function renderTemplate(event, container) {
+const renderTemplate = (templateId, container) => {
+    const template = document.querySelector(templateId);
+    const clone = template.content.cloneNode(true);
+    clean(container);
+    container.appendChild(clone);
+}
+const manageTagState = (target, title) => {
+    document.querySelectorAll(".tags").forEach(tag => tag.classList.remove("active"));
+    target.classList.add("active");
+    document.querySelector("title").textContent = title;
+}
+function manageTemplateState(event, container) {
     const target = event.target;
     if(target.classList.contains("tags")) {
         const title = target.dataset.title;
         const templateId = target.dataset.id;
 
-        document.querySelectorAll(".tags").forEach(tag => tag.classList.remove("active"));
-        target.classList.add("active");
+        manageTagState(target, title);
 
-        document.querySelector("title").textContent = title;
+        history.pushState({ id: templateId, title: title }, title, target.href);
 
-        const template = document.querySelector(templateId);
-        const clone = template.content.cloneNode(true);
-
-        history.pushState({ id: templateId }, title, target.href);
-
-        clean(container);
-        container.appendChild(clone);
+        renderTemplate(templateId, container);
     }
 }
 
-if ("content" in document.createElement("template")) {
+if("content" in document.createElement("template")) {
+    const listingSection = document.querySelector(".listing-section");
+    const tagContainer = document.querySelector(".tags-container");
+
     tagContainer.addEventListener("click", event => {
         event.preventDefault();
-        renderTemplate(event, listingSection);
+        manageTemplateState(event, listingSection);
+    });
+    window.addEventListener("popstate", event => {
+        if(!event.state) window.location = "/blog/";
+        const target = document.querySelector(`[data-title="${event.state.title}"]`);
+        manageTagState(target, event.state.title);
+        renderTemplate(event.state.id, listingSection);
     });
 }
