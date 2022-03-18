5c0903d81525f122a9f7f0a85f02020ef405a914
Mathieu Huot <mathieu.huot@gmail.com>
2022-03-18 14:23:54 -0400

    Refactored again.

--- esbuild/esbuild.js
+++ esbuild/esbuild.js
@@ -2,19 +2,20 @@ const esbuild = require("esbuild");
 const fs = require("fs");
 const path = require("path");
 
-const jsInputFolder = "js";
+const indir = "js";
+const outdir = "docs/js";
 
-const scripts = fs.readdirSync(jsInputFolder)
-.map(scriptPath => path.join(jsInputFolder, scriptPath))
-.filter(scriptPath => scriptPath !== path.join(jsInputFolder, "dependencies"));
+const scripts = fs.readdirSync(indir)
+.map(scriptPath => path.join(indir, scriptPath))
+.filter(scriptPath => scriptPath !== path.join(indir, "dependencies"));
 
 const isProd = process.env.ELEVENTY_ENV === "prod";
 
 esbuild.build({
-    entryPoints: scripts,
-    bundle: true,
-    format: "iife",
-    watch: !isProd,
-    minify: isProd,
-    outdir: "docs/js",
-  }).catch(() => process.exit(1));
+  entryPoints: scripts,
+  bundle: true,
+  format: "iife",
+  watch: !isProd,
+  minify: isProd,
+  outdir
+}).catch(() => process.exit(1));
