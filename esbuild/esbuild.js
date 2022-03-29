d05bc277a5abfad12b09c3aa45c3f12dbaae054c
Mathieu Huot <mathieu.huot@gmail.com>
2022-03-28 23:26:51 -0400

    Refactored js process.

--- esbuild/esbuild.js
+++ esbuild/esbuild.js
@@ -1,43 +1,26 @@
 const esbuild = require("esbuild");
-const browserslist = require("browserslist-to-esbuild")();
 
-const modernEntryPoint = "js/index.js";
-const tempLegacyEntryPoint = "_temp/js/legacy.js";
+const entryPoints = ["js/index.js"];
 const outdir = "docs/js";
 
 const isProd = process.env.ELEVENTY_ENV === "prod";
-const isBundlingLegacy = process.env.ESBUILD_ENV === "bundle_legacy";
-const isBundlingPolyfills = process.env.ESBUILD_ENV === "bundle_polyfills";
 
-if(!isBundlingPolyfills) {
-  esbuild.build({
-    entryPoints: [modernEntryPoint],
-    bundle: true,
-    format: "esm",
-    target: "es2015",
-    splitting: true,
-    watch: !isProd,
-    minify: isProd,
-    outdir
-  }).catch(() => process.exit(1));
-}
+esbuild.build({
+  entryPoints,
+  bundle: true,
+  format: "esm",
+  target: "es2015",
+  splitting: true,
+  watch: !isProd,
+  minify: isProd,
+  outdir
+}).catch(() => process.exit(1));
 
-if(isBundlingLegacy) {
+if(isProd) {
   esbuild.build({
-    entryPoints: [modernEntryPoint],
+    entryPoints,
     bundle: true,
-    target: browserslist,
     format: "iife"
-  }).catch(() => process.exit(1));
-}
-
-if(isBundlingPolyfills) {
-  esbuild.build({
-    entryPoints: [tempLegacyEntryPoint],
-    bundle: true,
-    target: browserslist,
-    format: "iife",
-    minify: true,
-    outdir
+    // Writes to stdout.
   }).catch(() => process.exit(1));
 }
